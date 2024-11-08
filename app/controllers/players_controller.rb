bc2f32af8ade9af57a98a18d334daf2ef8485fdd
dabrickashaw60 <121248625+dabrickashaw60@users.noreply.github.com>
2024-11-07 22:14:32 -0500

    Added in betting info and corrected last 5 averages

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -5,13 +5,33 @@ class PlayersController < ApplicationController
     @team = Team.find(params[:team_id])
     @player = @team.players.find(params[:id])
     @player_stats = @player.player_stats.order(season: :desc) # Assuming there's a `PlayerStat` model
-  
+    
     # Fetch all game logs for the player
     @game_logs = @player.box_scores.includes(:game).order('games.date DESC')
-  
+    
     # Fetch the last 5 game logs, most recent first
     @last_five_games = @game_logs.limit(5)
-  end  
+  
+    # Calculate averages for the last 5 games
+    @last_five_averages = {
+      minutes_played: calculate_average_minutes(@last_five_games),
+      points: average_stat(@last_five_games, :points),
+      rebounds: average_stat(@last_five_games, :total_rebounds),
+      assists: average_stat(@last_five_games, :assists),
+      field_goals: average_ratio(@last_five_games, :field_goals, :field_goals_attempted),
+      three_pointers: average_ratio(@last_five_games, :three_point_field_goals, :three_point_field_goals_attempted),
+      free_throws: average_ratio(@last_five_games, :free_throws, :free_throws_attempted),
+      plus_minus: average_stat(@last_five_games, :plus_minus)
+    }
+
+      # Betting info calculation
+      @betting_info = {
+        points: count_thresholds(@last_five_games, :points, [10, 15, 20, 25, 30]),
+        threes: count_thresholds(@last_five_games, :three_point_field_goals, [1, 2, 3, 4, 5]),
+        rebounds: count_thresholds(@last_five_games, :total_rebounds, [2, 4, 6, 8, 10]),
+        assists: count_thresholds(@last_five_games, :assists, [2, 4, 6, 8, 10])
+      }
+  end
 
   def update_stats
     @player = Player.find_by(id: params[:id])
@@ -33,6 +53,39 @@ class PlayersController < ApplicationController
     redirect_to team_path(@team, anchor: 'player-stats')
 
   end
+
+  private
+
+  def average_stat(games, stat)
+    games.sum(&stat).to_f / games.size
+  end
+
+  # Helper method to calculate the average minutes in "MM:SS" format
+  def calculate_average_minutes(games)
+    total_seconds = games.sum { |game| game.minutes_played.split(":").first.to_i * 60 + game.minutes_played.split(":").last.to_i }
+    average_seconds = total_seconds / games.size
+    minutes = average_seconds / 60
+    seconds = average_seconds % 60
+    format("%02d:%02d", minutes, seconds)
+  end
+
+  # Helper method to calculate field goal, three-point, and free throw averages in "makes / attempts" format
+  def average_ratio(games, made_stat, attempted_stat)
+    total_made = games.sum(&made_stat)
+    total_attempted = games.sum(&attempted_stat)
+    if total_attempted > 0
+      "#{(total_made.to_f / games.size).round(1)} / #{(total_attempted.to_f / games.size).round(1)}"
+    else
+      "0.0 / 0.0"
+    end
+  end
+
+  def count_thresholds(games, stat, thresholds)
+    thresholds.map do |threshold|
+      games.count { |game| game.send(stat) >= threshold }
+    end
+  end
+
 end
 
 
--- app/models/player.rb
+++ app/models/player.rb
@@ -8,5 +8,17 @@ class Player < ApplicationRecord
   
   validates :name, presence: true
   validates :from_year, :to_year, :position, :height, :weight, :birth_date, presence: true
+
+  def profile_picture_url
+    # Split the name by spaces to separate the first and last name components
+    name_parts = name.split
+    last_name = name_parts.last.downcase           # Use the last word as the last name
+    first_name = name_parts.first.downcase         # Use the first word as the first name
+
+    # Construct the URL
+    last_name_part = last_name[0, 5]               # Take the first 5 characters of the last name
+    first_name_part = first_name[0, 2]             # Take the first 2 characters of the first name
+    "https://www.basketball-reference.com/req/202106291/images/headshots/#{last_name_part}#{first_name_part}01.jpg"
+  end
   
 end
